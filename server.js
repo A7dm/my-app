0f30eafbb9e49bcb81a83417fc33a1630c4bed90
A7dm <a7dmxx@gmail.com>
2025-07-25 17:50:52 +0400

    Update server.js add port

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 // بيانات المعلمين فقط
 const teachers = [
